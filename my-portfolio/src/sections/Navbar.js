3f9cf09fe986a4cbfb5ae3b47e331eaadb228b9f
Vaishnavi-11 <vaishnavinagaraj790@gmail.com>
2021-08-07 13:37:57 +0530

    initial navbar.js

--- my-portfolio/src/sections/Navbar.js
+++ my-portfolio/src/sections/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { data } from '../data'
 
 function Navbar() {
     return (
         <div className="navbar">
-            <div></div>
+            <div className="navbar_content">
+                <div className="nav_left">
+                    <h1>Vaishnavi<span>.</span></h1>
+                </div>
+                <div className="nav_right">
+                    <a href="#about">About</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#contact">Contact</a>
+                    <a href={data.resume} target="_blank" id="resume">Resume</a>
+                </div>
+            </div>
         </div>
     )
 }
