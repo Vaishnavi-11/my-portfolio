74329e4e191dc506447a3937dd7f515e24b6c7df
Vaishnavi-11 <vaishnavinagaraj790@gmail.com>
2021-08-07 13:32:22 +0530

    just added structural elements

--- my-portfolio/src/sections/Skills.js
+++ my-portfolio/src/sections/Skills.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 function Skills() {
     return (
-        <div>
-            
+        <div id="skills">
+            <div className="about_heading">
+                <h2>About Me</h2>
+            </div>
+            <div className="about_content">
+                <h4>A little about me...</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia error dolores reprehenderit veniam alias recusandae autem. Totam quaerat ratione ex blanditiis id repellendus ut officia magni! Similique eius beatae, nisi explicabo amet quia nobis rem. Quia voluptatum nostrum dolorum a, cupiditate optio dicta nobis ipsam ipsa eveniet? Unde, magnam!</p>
+                <h4>Education</h4>
+                <p>B.E in Information Science and Engineering<br/>BMSCE</p>
+                <p>B.E in Information Science and Engineering<br/>BMSCE</p>
+                <p>B.E in Information Science and Engineering<br/>BMSCE</p>
+            </div>
         </div>
     )
 }
