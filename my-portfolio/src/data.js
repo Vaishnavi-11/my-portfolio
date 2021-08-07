ca69fcc41be36575135a8cb3decde488d3c2c72e
Vaishnavi-11 <vaishnavinagaraj790@gmail.com>
2021-08-07 13:35:52 +0530

    changed images to assests

--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -1,9 +1,11 @@
-import myImage from './images/me.jpeg'
+import myImage from './assets/me.jpeg'
+import myResume from './assets/VaishnaviN_Resume.pdf'
 
 export const data = {
     "name": "Vaishnavi N",
     "email": "vaishnavinagaraj790@gmail.com",
     "image": myImage,
     "role":"Information Science Undergrad",
-    "short_about":"An ambitious student , aspiring to become a full-fledged software developer."
+    "short_about":"An ambitious student , aspiring to become a full-fledged software developer.",
+    "resume": myResume
 }
