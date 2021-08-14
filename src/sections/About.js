53377d88d991f6e859677c40bc649c74eb1b4b36
Vaishnavi-11 <vaishnavinagaraj790@gmail.com>
2021-08-15 00:29:22 +0530

    designed about

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function About() {
+function About(props) {
     return (
         <div className="about" id="about">
             <div className="about_heading">
@@ -8,12 +8,50 @@ function About() {
                 <h2>About Me</h2>
             </div>
             <div className="about_content">
-                <h4>Hello!</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia error dolores reprehenderit veniam alias recusandae autem. Totam quaerat ratione ex blanditiis id repellendus ut officia magni! Similique eius beatae, nisi explicabo amet quia nobis rem. Quia voluptatum nostrum dolorum a, cupiditate optio dicta nobis ipsam ipsa eveniet? Unde, magnam!</p>
-                <h4>Education</h4>
-                <p>B.E in Information Science and Engineering<br/>BMSCE</p>
-                <p>B.E in Information Science and Engineering<br/>BMSCE</p>
-                <p>B.E in Information Science and Engineering<br/>BMSCE</p>
+                <div className="about_intro">
+                    <h4>Hello there!</h4>
+                    <p>Myself Vaishnavi and here's a little about me ....</p> 
+                    <p>I'm 20 years of age and am based in Bengaluru, Karnataka. 
+                        Currently a second-year student pursuing my Bachelors of Engineering Degree at B.M.S College of Engineering. 
+                        I am a dedicated and hard-working person. I enjoy coding and this is what drives me to explore and strenghten my knowledge.
+                        I have persistently been focusing on learning new frameworks and trying to work on the ones best suited to my interests and hope to continue the same </p>
+                    <p>Apart from the geeky side, my other hobbies would be listening to music (just like most of us xD). I do love to read
+                        books, sketch or play some video games during my free time.</p>
+                </div>
+                <div className="about_education">
+                    <h4>Education</h4>
+                    {
+                        props.education.map((education) => {
+                            return <ul key={education.id}  className="about_education_content">
+                                <li>
+                                    <div style={{display:'inline-block',verticalAlign:'top'}}>
+                                        <h5>{education.study}</h5>
+                                        <p>{education.institute}</p>
+                                        <p>{education.year}</p>
+                                        <p>{education.grade}</p>
+                                        <p>{education.accomplishment}</p>
+                                    </div>
+                                </li>
+                            </ul>
+                        })
+                    }
+                </div>
+                <div className="about_certificates">
+                    <h4>Certifications</h4>
+                    {
+                        props.certification.map((certificate) => {
+                            return <ul key={certificate.id}  className="about_certificates_content">
+                                <li>
+                                    <div style={{display:'inline-block',verticalAlign:'top'}}>
+                                        <h5>{certificate.course_name}</h5>
+                                        <p>{certificate.platform} | {certificate.institute}</p>
+                                        <p>{certificate.date}</p>
+                                    </div>
+                                </li>
+                            </ul>
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
