7bcbea9e2969d2cfbf0f9f0359d5cc201d133409
Vaishnavi-11 <vaishnavinagaraj790@gmail.com>
2021-08-15 00:36:41 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import App from './App';
 
 ReactDOM.render(
